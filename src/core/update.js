1d69ee93cfd974944d7282b1ff91a0924c12812f
Bob Haslett <bob.haslett@ft.com>
2023-09-29 17:57:54 +0100

    end of day clean up

--- src/core/update.js
+++ src/core/update.js
@@ -55,168 +55,167 @@ const positionLabels = (labels, spacing, alpha) => {
 export default function update() {
   const { colors, layout, chart, data, state, facets, props, } = this;
 
-// /////////// DATA
+  // /////////// DATA
 
-// Conditionally maps the facet names depending on if a grod of charts or a single plot is requires
-const facetNames = state.gridKey ? data.Lines.map( d => d.facet)
-.filter((item, pos, facetNames) => facetNames.indexOf(item) === pos)
-: [""]
-const columnNames = data.polls.column_names.value;
-colors.updateColorScale(columnNames);
+  // Conditionally maps the facet names depending on if a grod of charts or a single plot is requires
+  const facetNames = state.gridKey ? data.Lines.map( d => d.facet)
+  .filter((item, pos, facetNames) => facetNames.indexOf(item) === pos)
+  : [""]
+  const columnNames = data.polls.column_names.value;
+  colors.updateColorScale(columnNames);
 
-const { displayData } = this.data;
+  const { displayData } = this.data;
 
-const { dateFormat } = state;
-const parseDate = d3.timeParse(dateFormat);
+  const { dateFormat } = state;
+  const parseDate = d3.timeParse(dateFormat);
 
-// Format the polling data so that it can be used to create global values and plot points
-const pollData = data.polls
-  .map((d, i) => {
-    const row = { date: parseDate(d.date), pollster: d.pollster, rowID: i, };
-    columnNames.forEach((el, i) => {
-       row[columnNames[i]] = Number.isNaN(d.value[i]) ? "" : Number(d.value[i]);
-    });
-    return row;
+  // Format the polling data so that it can be used to create global values and plot points
+  const pollData = data.polls
+    .map((d, i) => {
+      const row = { date: parseDate(d.date), pollster: d.pollster, rowID: i, };
+      columnNames.forEach((el, i) => {
+        row[columnNames[i]] = Number.isNaN(d.value[i]) ? "" : Number(d.value[i]);
+      });
+      return row;
+    })
+    .sort((a, b) => a.date - b.date);
+
+  const linesData = data.Lines.map((d) => {
+    return {
+      date: parseDate(d.date),
+      party: d.party,
+      lower: Number(d.lower),
+      upper: Number(d.upper),
+      value: Number(d.value)
+    }
   })
   .sort((a, b) => a.date - b.date);
 
-const linesData = data.Lines.map((d) => {
-  return {
-    date: parseDate(d.date),
-    party: d.party,
-    lower: Number(d.lower),
-    upper: Number(d.upper),
-    value: Number(d.value)
+  // Create a global date extent array as this remains constant across all facets
+  const dateExtent = d3.extent(pollData, (d) => d.date);
+    // Check for user overideas to the dateextent array
+    dateExtent[0] = state.x.datetime_min
+      ? new Date(state.x.datetime_min)
+      : d3.extent(linesData, (d) => d.date)[0];
+    dateExtent[1] = state.x.datetime_max
+      ? new Date(state.x.datetime_max)
+      : d3.extent(linesData, (d) => d.date)[1];
+
+  // calculate and apply fixed height on breakpoint before rem calculation
+  // or text on bars will jump when cross the tablet breakpoint. This is also used to determine the correct Formatdisplayname
+  const breakpoint = state.layout.breakpoint_tablet;
+  // update the proportions of the containing svg
+  let width;
+  let height;
+  // Use the layout setHeight functionality to control the aspect ration when .ration selected
+  if (this.state.aspectRatio === "ratio") {
+    width = layout.getPrimaryWidth();
+    // Use the breakpoint to determne which aspect ratio calculation is used
+    height =
+      width <= breakpoint
+        ? width / state.aspect.small
+        : width / state.aspect.desk;
+    this.layout.setHeight(height);
+  } else {
+    width = layout.getPrimaryWidth();
+    height = layout.getPrimaryHeight();
+    layout.setHeight(null);
   }
-})
-.sort((a, b) => a.date - b.date);
+  chart
+    .attr('width', width)
+    .attr('height', height)
+  layout.update();
 
-// Create a global date extent array as this remains constant across all facets
-const dateExtent = d3.extent(pollData, (d) => d.date);
-  // Check for user overideas to the dateextent array
-  dateExtent[0] = state.x.datetime_min
-    ? new Date(state.x.datetime_min)
-    : d3.extent(linesData, (d) => d.date)[0];
-  dateExtent[1] = state.x.datetime_max
-    ? new Date(state.x.datetime_max)
-    : d3.extent(linesData, (d) => d.date)[1];
-
-// calculate and apply fixed height on breakpoint before rem calculation
-// or text on bars will jump when cross the tablet breakpoint. This is also used to determine the correct Formatdisplayname
-const breakpoint = state.layout.breakpoint_tablet;
-// update the proportions of the containing svg
-let width;
-let height;
-// Use the layout setHeight functionality to control the aspect ration when .ration selected
-if (this.state.aspectRatio === "ratio") {
-  width = layout.getPrimaryWidth();
-  // Use the breakpoint to determne which aspect ratio calculation is used
-  height =
-    width <= breakpoint
-      ? width / state.aspect.small
-      : width / state.aspect.desk;
-  this.layout.setHeight(height);
-} else {
-  width = layout.getPrimaryWidth();
-  height = layout.getPrimaryHeight();
-  layout.setHeight(null);
-}
-chart
-  .attr('width', width)
-  .attr('height', height)
-layout.update();
-
-// define an array to contain the line labels
- const labels = []
- // Conditionally fill the labels array with correct line label depending of chart width
- columnNames.forEach (party => {
-   const viewLabel = displayData.find(({ party: p }) => party === p);
-   if(width < breakpoint) {
-     labels.push(viewLabel.displayNameMobile)
-   }
-   labels.push(viewLabel.displayNameDesktop)
- })
-
-const facetData = facetNames.map((facetName) => {
-
-  // Create a unique list of parties that are only plotted in this particular facet
-  const parties =state.gridKey
-  ? data.Lines
-    .filter((row) =>  row.facet === facetName)
-    .map( d => d.party)
-    .filter((item, pos, parties) => parties.indexOf(item) === pos)
-  : columnNames
-
-  // Build the plot object containing data to be rendered for each facet
-  const plotData = parties.map((party) => {
-
-    const viewData = displayData.find(({ party: p }) => party === p);
-    // Filter the lines data so tha just those with the parties for this facet are plotted
-    const plotLines = linesData
-    .filter((lineRow) => {
-      return lineRow.party === party;
-    })
-    return {
-      party,
-      displayNameMob: viewData.displayNameMobile,
-      displayNameDesk: viewData.displayNameDesktop,
-      textColor: viewData.altTextColor,
-      dots: getDots(pollData, party),
-      areas: getMoE(plotLines, party),
-    };
-
+  // define an array to contain the line labels
+  const labels = []
+  // Conditionally fill the labels array with correct line label depending of chart width
+  columnNames.forEach (party => {
+    const viewLabel = displayData.find(({ party: p }) => party === p);
+    if(width < breakpoint) {
+      labels.push(viewLabel.displayNameMobile)
+    }
+    labels.push(viewLabel.displayNameDesktop)
   })
 
-  return {
-    name: facetName,
-    parties: parties, //list of the parties to be displayed in that particular facet
-    plotData: plotData,
-  }
-})
+  const facetData = facetNames.map((facetName) => {
 
-// //// RENDER
+    // Create a unique list of parties that are only plotted in this particular facet
+    const parties =state.gridKey
+    ? data.Lines
+      .filter((row) =>  row.facet === facetName)
+      .map( d => d.party)
+      .filter((item, pos, parties) => parties.indexOf(item) === pos)
+    : columnNames
 
-// Generate condition to be used to test if differing scales are needed on the y scale
-const sameY = state.facets.sameY
+    // Build the plot object containing data to be rendered for each facet
+    const plotData = parties.map((party) => {
+      // Returns an object containing the party disply labels and text colours
+      const viewData = displayData.find(({ party: p }) => party === p);
+      
+      // Filter the lines data so tha just those with the parties for this facet are plotted
+      const plotLines = linesData
+      .filter((lineRow) => {
+        return lineRow.party === party;
+      })
+      return {
+        party,
+        displayNameMob: viewData.displayNameMobile,
+        displayNameDesk: viewData.displayNameDesktop,
+        textColor: viewData.altTextColor,
+        dots: getDots(pollData, party),
+        areas: getMoE(plotLines, party),
+      };
+    })
 
-facets
-  .width(width)
-  .height(height)
-  .data(facetData, d => d.name)
-  .update((facet) => {
-    
-
-    //Conditionally generate the range for the x axis depending on if the same values are wanted across each facet
-    const pollExtent = sameY ? extentMulti(pollData, columnNames)
-    : extentMulti(pollData, facet.data.parties);
-    const lineExtent = sameY ?  extentMulti(linesData, ['lower', 'upper'])
-    : extentMulti(linesData.filter((row) => {return facet.data.parties.includes(row.party)}), ['lower', 'upper']);
-
-    const valueExtent = [(Math.min(pollExtent[0],lineExtent[0])), (Math.max(pollExtent[1],lineExtent[1]))]
-    valueExtent[0] = state.y.linear_min
-    ? state.y.linear_min
-    : valueExtent[0];
-    valueExtent[1] = state.y.linear_max
-    ? state.y.linear_max
-    : valueExtent[1];
-
-    //Chart layout created and updated if none exists. Nees to be updated before anything else so that te REM val;ue can be returned
-    if (!facet.node.chartLayout)  {
-      facet.node.chartLayout = createChartLayout(facet.node, props).update();
+    return {
+      name: facetName,
+      parties: parties, //list of the parties to be displayed in that particular facet
+      plotData: plotData,
     }
+  })
 
-    const rem = (layout.remToPx(100) / 100) * state.x.tick_label_size;
-    const tickFotmat = state.tickFormat;
-    const labelTuine = state.tuneLabel;
-    const numberWidth = getTextWidth(" WW.W", `${rem}px MetricWeb`); // Additional value that allows for the max width of figures on the end of the label
-    
-     // Calculate the new right hand margin toallow for the labels, this is kept common accross all facets so that y axis align in the grid
-     const rightLabelWidth =
-     getMaxTextWidth(labels, `${rem}px MetricWeb`) +
-     numberWidth +
-     labelTuine;
+  // //// RENDER
 
-    facet.node.chartLayout
+  // Generate condition to be used to test if differing scales are needed on the y scale
+  const sameY = state.facets.sameY
+
+  facets
+    .width(width)
+    .height(height)
+    .data(facetData, d => d.name)
+    .update((facet) => {
+
+  //Conditionally generate the range for the x axis depending on if the same values are wanted across each facet
+  const pollExtent = sameY ? extentMulti(pollData, columnNames)
+  : extentMulti(pollData, facet.data.parties);
+  const lineExtent = sameY ?  extentMulti(linesData, ['lower', 'upper'])
+  : extentMulti(linesData.filter((row) => {return facet.data.parties.includes(row.party)}), ['lower', 'upper']);
+
+  const valueExtent = [(Math.min(pollExtent[0],lineExtent[0])), (Math.max(pollExtent[1],lineExtent[1]))]
+  valueExtent[0] = state.y.linear_min
+  ? state.y.linear_min
+  : valueExtent[0];
+  valueExtent[1] = state.y.linear_max
+  ? state.y.linear_max
+  : valueExtent[1];
+
+  //Chart layout created and updated if none exists. Nees to be updated before anything else so that te REM val;ue can be returned
+  if (!facet.node.chartLayout)  {
+    facet.node.chartLayout = createChartLayout(facet.node, props).update();
+  }
+  // Returns the rem size to be the same as the x axis tick label
+  const rem = (layout.remToPx(100) / 100) * state.x.tick_label_size;
+  const tickFotmat = state.tickFormat; // user defined x axis date format
+  const labelTuine = state.tuneLabel; // Fine tunes the lable spacing on the lines
+  const numberWidth = getTextWidth(" WW.W", `${rem}px MetricWeb`); // Additional value that allows for the max width of figures on the end of the label
+  
+  // Updates the right hand margin based on the width of the longest label, this is kept common accross all facets so that y axis align in the grid
+  const rightLabelWidth =
+  getMaxTextWidth(labels, `${rem}px MetricWeb`) +
+  numberWidth +
+  labelTuine;
+
+  facet.node.chartLayout
     .width(facet.width)
     .height(facet.height)
     .xData(dateExtent)
@@ -226,19 +225,17 @@ facets
       // Blank update is called so that the scales are initiated.
       // Also margin values have to be passed EVERY time chartLayout update is called
     )
-   //Get the scales from the updated chartLayout
-    const yScale = facet.node.chartLayout.yScale();
-    const xScale = facet.node.chartLayout.xScale();
-    console.log('STATE', state.x.tick_label_size)
-    // Calculates the rem size to be the same as the x axis tick label
+  //Get the scales from the updated chartLayout
+  const yScale = facet.node.chartLayout.yScale();
+  const xScale = facet.node.chartLayout.xScale();
 
-    // Rewturns the lastt plotted date
-    const lastDate =
-    new Date(state.x.datetime_max) <
-    new Date(d3.extent(pollData, (d) => d.date)[1])
-      ? new Date(state.x.datetime_max)
-      : d3.extent(pollData, (d) => d.date)[1];
-    
+  // Rewturns the lastt plotted date
+  const lastDate =
+  new Date(state.x.datetime_max) <
+  new Date(d3.extent(pollData, (d) => d.date)[1])
+    ? new Date(state.x.datetime_max)
+    : d3.extent(pollData, (d) => d.date)[1];
+      
   // function to check if the last plotted date falls before the end of the x axis date range to adjust the right margin labelwidth accordingly
   let newMargin;
   const maxXDate = facet.node.chartLayout.xData().max;
@@ -254,14 +251,16 @@ facets
     {margins: { right: newMargin }},
     renderFacets()
   )
-    
+      
   // Function that draws the each chart in the grid
   function renderFacets() { 
 
+    //Return the plotData for this facet
     const facetPlotData = facet.data.plotData
     
-    console.log('facetData', facetData )
+    // Return the  scg plot object
     const plot = d3.select(facet.node);
+
     // Assign the various rendering options for the lines, dots and areas
     const dotOpacity =
       width < breakpoint
@@ -382,8 +381,6 @@ facets
       .attr("fill", (d) => colors.getColor(d.party))
       .attr("opacity", state.polls.render ? dotOpacity : 0)  
     
-      console.log('facetPlotData',facetPlotData)
-
     // Set up label data
     const labelData = facetPlotData
       .map(({ areas, party, displayNameDesk, displayNameMob, textColor }) => {
@@ -399,9 +396,9 @@ facets
         };
       })
       .sort((a, b) => b.average - a.average);
-    console.log('labelData', labelData)
     
-    const labelHolder = plot
+    // Create a group for each label
+    plot
       .selectAll(".labelHolder")
       .data(labelData)
       .enter()
@@ -415,7 +412,7 @@ facets
       0.5 // Amount to change label positon by each iteration
     );
     
-    // Add the label
+    // Add the label rects
     plot
       .selectAll(".labelHolder")
       .selectAll("rect")
@@ -425,9 +422,7 @@ facets
           enter
             .append("rect")
             .attr("height", rem)
-            .attr("y", (d) => {
-              console.log(rem)
-              return d.position - rem * 0.5})
+            .attr("y", d => d.position - rem * 0.5)
             .attr("x", () => xScale(lastDate) + rem * 0.3),
         (updateRect) =>
         updateRect
@@ -483,15 +478,10 @@ facets
         }
         return `${d.displayNameDesk} ${formatLabel(d.average)}`;
       });
-      console.log(colors)
-      console.log(colors.categorical_custom_palette)
 
   }
 
-
   });
-
-// Hides the facet title on single charts
-facets.hideTitle(facetNames[0])
-
+  // Hides the facet title on single charts
+  facets.hideTitle(facetNames[0])
 }

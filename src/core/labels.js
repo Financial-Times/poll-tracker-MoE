e30d4b3809a641cdef9fb2cd034d520831a19353
eadehemingway <eade.hemingway@gmail.com>
2024-06-26 16:03:06 -0400

    format file

--- src/core/labels.js
+++ src/core/labels.js
@@ -1,95 +1,97 @@
 import * as d3 from "d3";
 
-export function updateLabels({facet, state, rem, isMobile, labelData, colors, xScale, lastDate}){
-    // Return the  scg plot object
-    const plot = d3.select(facet.node)
-    // Create a group for each label
+export function updateLabels({
+  facet,
+  state,
+  rem,
+  isMobile,
+  labelData,
+  colors,
+  xScale,
+  lastDate,
+}) {
+  // Return the  scg plot object
+  const plot = d3.select(facet.node);
+  // Create a group for each label
+  plot
+    .selectAll(".labelHolder")
+    .data(labelData)
+    .enter()
+    .append("g")
+    .attr("class", "labelHolder");
+
+  // Calculate new label positions recursively
+  positionLabels(
+    plot.selectAll(".labelHolder"),
+    rem, // Minimum spacing between labels (increase for more space)
+    0.5 // Amount to change label positon by each iteration
+  );
+
+  // Add the label rects
+  const thirdRem = rem * 0.3;
+  if (!isMobile) {
     plot
       .selectAll(".labelHolder")
-      .data(labelData)
-      .enter()
-      .append("g")
-      .attr("class", "labelHolder");
-    
-    // Calculate new label positions recursively
-    positionLabels(
-      plot.selectAll(".labelHolder"),
-      rem, // Minimum spacing between labels (increase for more space)
-      0.5 // Amount to change label positon by each iteration
-    );
-
-    // Add the label rects
-    const thirdRem = rem * 0.3
-    if (!isMobile){
-      plot
-        .selectAll(".labelHolder")
-        .selectAll("rect")
-        .data((d) => [d])
-        .join(
-          (enter) =>
-            enter
-              .append("rect")
-              .attr("height", rem),
-          (updateRect) =>
-          updateRect
-              .attr("y", (d) => d.position - rem * 0.5)
-              .attr("x", () => xScale(lastDate) + thirdRem)
-              .attr("width", rem * 0.5),
-          (exit) =>
-            exit
-              .transition()
-              .attr("width", 0)
-              .on("end", function labelHolderRectExitOnEnd() {
-                d3.select(this).remove();
-              })
-        )
-        .attr("fill", (d) => colors.getColor(d.party))
-        .attr("height", rem)
-        .attr("width", rem * 0.5);
-    }
-
-    // Formatting for the label number
-    const formatLabel = d3.format(".1f");
-
-    const labelOffset = !isMobile ? rem : thirdRem
-    // add the party name labels
-    plot
-      .selectAll(".labelHolder")
-      .selectAll("text")
+      .selectAll("rect")
       .data((d) => [d])
       .join(
-        (enter) =>
-          enter
-            .append("text"),
-        (updateText) =>
-        updateText
-            .attr("y", (d) => d.position + thirdRem)
-            .attr("x", () => xScale(lastDate) + labelOffset),
+        (enter) => enter.append("rect").attr("height", rem),
+        (updateRect) =>
+          updateRect
+            .attr("y", (d) => d.position - rem * 0.5)
+            .attr("x", () => xScale(lastDate) + thirdRem)
+            .attr("width", rem * 0.5),
         (exit) =>
           exit
             .transition()
-            .attr("opacity", 0)
-            .on("end", function labelHolderTextExitOnEnd() {
+            .attr("width", 0)
+            .on("end", function labelHolderRectExitOnEnd() {
               d3.select(this).remove();
             })
       )
-      .attr("font-weight", 600)
-      .attr("font-size", rem)
-      .style("fill", (d) => {
-        // Conditionally set the color of the text depending on if an alternative colour is defined
-        return d.altTextColor || colors.getColor(d.party)
-      })
-      .text((d) => {
-        if (isMobile) {
-          const mobileDisplayName = d.displayNameMob || ""
-          const text = state.showLegendOnMobile ?  "" : mobileDisplayName
-          return `${text} ${formatLabel(d.average)}`;
-        }
-        return `${d.displayNameDesk} ${formatLabel(d.average)}`;
-      });
-
+      .attr("fill", (d) => colors.getColor(d.party))
+      .attr("height", rem)
+      .attr("width", rem * 0.5);
   }
 
+  // Formatting for the label number
+  const formatLabel = d3.format(".1f");
+
+  const labelOffset = !isMobile ? rem : thirdRem;
+  // add the party name labels
+  plot
+    .selectAll(".labelHolder")
+    .selectAll("text")
+    .data((d) => [d])
+    .join(
+      (enter) => enter.append("text"),
+      (updateText) =>
+        updateText
+          .attr("y", (d) => d.position + thirdRem)
+          .attr("x", () => xScale(lastDate) + labelOffset),
+      (exit) =>
+        exit
+          .transition()
+          .attr("opacity", 0)
+          .on("end", function labelHolderTextExitOnEnd() {
+            d3.select(this).remove();
+          })
+    )
+    .attr("font-weight", 600)
+    .attr("font-size", rem)
+    .style("fill", (d) => {
+      // Conditionally set the color of the text depending on if an alternative colour is defined
+      return d.altTextColor || colors.getColor(d.party);
+    })
+    .text((d) => {
+      if (isMobile) {
+        const mobileDisplayName = d.displayNameMob || "";
+        const text = state.showLegendOnMobile ? "" : mobileDisplayName;
+        return `${text} ${formatLabel(d.average)}`;
+      }
+      return `${d.displayNameDesk} ${formatLabel(d.average)}`;
+    });
+}
 
 // Helper function to position labels
 const positionLabels = (labels, spacing, alpha) => {
